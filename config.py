61e1997821c2d7eb2a12e78015c9cae609d5390e
PAPA <sachinxxxsanatani@gmail.com>
2024-08-18 09:10:35 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 7374531519))
-
+OWNER_USERNAME = int(getenv("OWNER_USERNAME", 5959548791))
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
