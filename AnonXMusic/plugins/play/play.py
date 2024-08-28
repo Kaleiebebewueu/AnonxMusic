a850e8f057c702c0c7e86a44b400f9c5cef4b88d
PAPA <sachinxxxsanatani@gmail.com>
2024-08-28 19:13:41 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -28,7 +28,7 @@ from config import BANNED_USERS, lyrical
 @app.on_message(
     filters.command(
         [
-            "isplay",
+            "play",
             "vplay",
             "cplay",
             "cvplay",
