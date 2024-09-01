b33cddd684b107b192906c0385af72fa581034b4
PAPA <sachinxxxsanatani@gmail.com>
2024-09-01 22:32:46 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- AnonXMusic/help/buttons.py
+++ AnonXMusic/help/buttons.py
@@ -4,11 +4,11 @@ from pyrogram import Client, filters, enums
 class BUTTONS(object):
     MBUTTON = [
               [
-                  InlineKeyboardButton("+", callback_data="mplus HELP_HELP1"),
-                  InlineKeyboardButton("+", callback_data="mplus HELP_HELP2"),
-                  InlineKeyboardButton("+", callback_data="mplus HELP_HELP3")
+                  InlineKeyboardButton("+", callback_data="music HELP_HELP1"),
+                  InlineKeyboardButton("+", callback_data="music HELP_HELP2"),
+                  InlineKeyboardButton("+", callback_data="music HELP_HELP3")
                ],
                [
                   InlineKeyboardButton("⩤", callback_data=f"settings_back_helper"), 
-                  InlineKeyboardButton("⩥", callback_data=f"managebot123 settings_back_helper"),
+                  InlineKeyboardButton("⩥", callback_data=f"sanatani settings_back_helper"),
                ]]
